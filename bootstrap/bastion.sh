ebf54767dd9a4048f6614b4399302c6e81a6a328
Jevhenij Zacharcenko <zevgan@gmail.com>
2026-01-31 18:07:04 -0500

    disable ipv6

--- bootstrap/bastion.sh
+++ bootstrap/bastion.sh
@@ -78,6 +78,13 @@ systemctl enable --now wg-quick@wg0
 cat > /etc/sysctl.d/99-infrazero-forward.conf <<'EOF'
 net.ipv4.ip_forward=1
 EOF
+
+# Disable IPv6 on bastion
+cat > /etc/sysctl.d/99-infrazero-disable-ipv6.conf <<'EOF'
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1
+EOF
 sysctl --system
 
 detect_private_if() {
